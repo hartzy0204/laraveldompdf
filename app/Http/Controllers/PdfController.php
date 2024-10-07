83a42892b947c8fc2ffd8a18bf2b5d21dcb44c2d
hartzy0204 <harveybarace.main@gmail.com>
2024-10-07 09:26:31 -0700

    storage link

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -12,21 +12,16 @@ class PdfController extends Controller
     public function index($id)
     {
 
-        // dd($request->all());
 
        $user = User::findOrFail($id);
 
 
-        // Load the Blade view and pass data to it
         $pdf = PDF::loadView('pdf_template', compact('user'));
 
-        // Define a filename for the PDF
         $fileName = 'document.pdf';
 
-        // Save the PDF to the storage directory
         Storage::put('public/pdfs/' . $fileName, $pdf->output());
 
-        // Stream the PDF to the browser
         return $pdf->stream($fileName);
     }
 }
--- resources/views/pdf_template.blade.php
+++ resources/views/pdf_template.blade.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+ <!-- php artisan storage:link -->
+    <img src="{{ public_path('storage/1228259.jpeg') }}" alt="">
     <div style="position: absolute; left: 50px">{{$user['name']}}</div>
 </body>
+
 </html>
